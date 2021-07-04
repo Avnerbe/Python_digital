96423689c102708c566917536de4de2b14aab58f
avner <avnerb40@gmail.com>
2021-07-04 11:50:02 +0300

    python syntax

---
+++ lesson1/targil2.py
@@ -0,0 +1,20 @@
+a=12
+b=11.56
+c="Avner Belisha"
+
+#this is integer
+print(a)
+print(type(a))
+
+#this is fload
+print(b)
+print(type(b))
+
+#this is string
+print(c)
+print(type(c))
+
+print("hello " + c+"\nhow are you?")
+
+mail= input("your mail is:")
+print (mail)
