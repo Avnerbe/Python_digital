b2b4ec4aa58cc6672529a6dea713f4ce3e9f4d7a
avner <avnerb40@gmail.com>
2021-07-08 23:34:02 +0300

    Final exem part2

--- Final exem part2.py
+++ Final exem part2.py
@@ -2,34 +2,60 @@ from random import randint
 from time import sleep
 
 
-#row_cost=3
-money=(int(input("How much money do you have: " )))
+
+money=(int(input("         hello \n       --------\n     row_cost=3 ILS\nHow much money do you have: " )))
 x=money//3
-print ("you can play: "+str(x)+" rounds\n")
+print ("you can play: "+str(x)+" rows\n  ---------------\n")
+if(x==0):
+    print ("bye bye")
+else:
+    total_winning = 0
+    #הגרלת 6 מספרים ע"פ מספר הסיבובים שמאפשר הכסף
+    for rounds in range(x):
+        list = []
+        my_number=[]
+        for y in range(6):
+            y=(int(input("please choose your lottory number (between 1-37): ")))
+            if (x>37 or x<1):
+                x = (int(input("sorry invalid number please choose new number :" )))
+                my_number = my_number + [y]
+            else:
+                my_number=my_number + [y]
 
+        print("\nyour lottory numbers are: \n  ---------------\n"+ str(my_number))
 
-#הגרלת 6 מספרים ע"פ מספר הסיבובים שמאפשר הכסף
-for term in range(x):
-    list = []
-    my_num = ["8", "21", "2", "5","6", "11"]
-    for win_numbers in range(6):
-      num=randint(1,37)
-      #ביטול כפילות מספרים
-      if(num in list):
-          num = randint(1,37)
-          #בדיקת זכיה
-          if(str(num) in my_num):
-              print("winning")
-          else:
-              #win=0
-              #winning =win+0
-              print("no win")
-          list = list + [num]
-      else:
-          list = list + [num]
+        win_numbers=[]
+        win=0
+        for num in range(6):
+            num=randint(1,37)
+            win_numbers= win_numbers +[num]
+            if(num in my_number):
+                win=win+1
+            else:
+                continue
+        print ("\n\nThe winning numbers are: \n  ---------------\n"+str(win_numbers))
+        print("\n  ---------------\nyour guess: "+str(win)+ " numbers")
+        #חישובי זכיה
+        prize=0
+        #total_winning=0
+        if (win==3):
+            prize=prize+10
+            print("you won: " +str(prize)+" ILS\n")
+        elif(win==4):
+            prize = prize + 100
+            print("you won: " +str(prize)+" ILS\n")
+        elif (win == 4):
+            prize = prize + 5000
+            print("you won: " +str(prize)+" ILS\n")
+        elif (win == 4):
+            prize = prize + 1000000
+            print("you won: " +str(prize)+" ILS\n")
+        else:
+            prize = 0
+            print("you won: " +str(prize)+" ILS\n")
 
-    print(list)
-
+        total_winning =total_winning+prize
+    print("your total winning:" +str(total_winning)+" ILS\n")
 
 
 
